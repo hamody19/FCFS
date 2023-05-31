bf7bc499ea09d43ea132cdb26e86ed1ed08db093
hamody19 <mohammadalslamy@hotmail.com>
2023-05-31 18:44:03 +0300

    make the code more simple

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from printFcfs import print_fcfs
 from threading import Thread
 if __name__ == "__main__":
     processes = [
-        Process("p1", 0, 15),
-        Process("p2", 2, 6),
-        Process("p3", 3, 7),
-        Process("p4", 5, 5)
+        Process("p1", p_arrival_time=0, p_burst_time=15),
+        Process("p2", p_arrival_time=2, p_burst_time=6),
+        Process("p3", p_arrival_time=3, p_burst_time=7),
+        Process("p4", p_arrival_time=5, p_burst_time=5)
     ]
     thread = Thread(target=fcfs(processes))
     thread.start()
